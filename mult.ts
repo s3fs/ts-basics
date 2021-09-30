a639525c99e4f5e29dce99ec8194ae2ccfcf3b57
s3fs <northfacebodybags@yahoo.com>
2021-09-30 19:10:20 +0500

    calc

--- mult.ts
+++ mult.ts
@@ -1,5 +1,16 @@
-const multiplicator = (a: number, b: number, printTxt: string) => {
-  console.log(printTxt, a * b);
+type Operation = 'multiply' | 'add' | 'divide';
+type Result = string | number;
+
+const calc = (a: number, b: number, op: Operation): Result => {
+  if (op === 'multiply') {
+    return a * b;
+  } else if (op === 'add') {
+    return a + b;
+  } else if (op === 'divide') {
+    if (b === 0) return 'can\'t divide by 0';
+    return a / b;
+  }
+  return 'hehe'
 }
 
-multiplicator(2, 4, 'multip 2x4');
+console.log(calc(2, 10, 'add'))
