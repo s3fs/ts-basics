186a3007eb5cfd96a11cda2113e02765f581283e
s3fs <northfacebodybags@yahoo.com>
2021-10-03 01:26:09 +0500

    mul argv refine

--- mul.ts
+++ mul.ts
@@ -1,8 +1,33 @@
-const mul = (a: number, b: number, printTxt: string) => {
-  console.log(printTxt, a * b)
+interface MulVals {
+  val1: number;
+  val2: number;
 }
 
-const num1: number = Number(process.argv[2])
-const num2: number = Number(process.argv[3])
+const parseArgv = (args: Array<string>): any => {
+  const t = (x: string): Error => {
+    throw new Error(x);
+  }
 
-mul(num1, num2, `${num1} x ${num2} results in `)
+  if (args.length < 4) t('Not enough arguments');
+  if (args.length > 4) t('Too many arguments');
+
+  if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+    return {
+      val1: Number(args[2]),
+      val2: Number(args[3])
+    }
+  } else {
+    t('Provided values are not Numbers');
+  }
+}
+
+const mul = (a: number, b: number, printTxt: string) => {
+  console.log(printTxt, a * b);
+}
+
+try {
+  const { val1, val2 } = parseArgv(process.argv);
+  mul(val1, val2, `${val1} x ${val2} results in `);
+} catch (e) {
+  console.log('Error ', e)
+}
