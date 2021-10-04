32102dd0ddd0e38e936f62758a6955b6e62f9b3b
s3fs <northfacebodybags@yahoo.com>
2021-10-04 14:03:10 +0500

    9.3 1/2

--- index.ts
+++ index.ts
@@ -1,4 +1,22 @@
-const calcBmi = (h: number, w: number): number | string => {
+interface Parsed {
+  val1: number
+  val2: number
+}
+
+const parse = (argv: Array<string>): Parsed => {
+  if (argv.length < 4) {
+    throw new Error('Provide height and weight values')    
+  } else if (isNaN(Number(argv[2])) || isNaN(Number(argv[3]))) {
+    throw new Error('Provided values are not numbers')
+  }
+
+  return {
+    val1: Number(argv[3]),
+    val2: Number(argv[2])
+  }
+}
+
+const calcBmi = (w: number, h: number): number | string => {
   const res: number =  w / ((h / 100) ** 2)
   
   if (res < 18.5) {
@@ -10,4 +28,9 @@ const calcBmi = (h: number, w: number): number | string => {
   }
 }
 
-console.log(calcBmi(180, 70))
+try {
+  const { val1, val2 } = parse(process.argv)
+  console.log(calcBmi(val1, val2))
+} catch (e) {
+  console.log(e)
+}
